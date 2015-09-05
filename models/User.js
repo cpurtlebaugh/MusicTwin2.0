6b969b726b9d1b7421eee77b018209106093e9b5
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-09-05 15:29:11 -0700

    1. Add server.js file and included dependecies, set up db, run express and set up error handling. 2. Start User model 3. Updated package.json 4. Updated bin file to include server.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,29 @@
-User.js
+var mongoose    = require('mongoose'),
+    Schema      = mongoose.Schema,
+    bcrypt      = require('bcrypt-nodejs');
+
+
+var UserSchema  = new Schema ({
+    firstName   = String,
+    lastName    = String,
+    email       = String,
+    username    = {type: String, required: true, index: {unique: true}},
+    password    = {type: String, required: true, select: false,
+});
+
+// hash the pw before the user is saved
+UserSchema.pre('save', function(){
+  var user = this;
+
+  // has only if the user is new or a current user has changed it
+  if (!user.isModified('password')) return next();
+
+  // generate the hash
+
+  bcrypt.hash(user.password, null, null, function(err, hash){
+    if (err) return next(err);
+    // change pw to the hashed version
+    user.password = hash;
+    next();
+  });
+});
---
+++ server.js
@@ -0,0 +1,75 @@
+// requiring / loading all dependencies
+var express         = require ('express'),
+    path            = require ('path'),
+    favicon         = require ('serve-favicon'),
+    logger          = require ('morgan'),
+    cookieParser    = require ('cookie-parser'),
+    bodyParser      = require ('body-parser');
+
+// check that MongoD is running
+require('net').connect(27017, 'localhost').on('error', function(){
+  console.log("you must BOW before da MongoD yo!");
+  process.exit(0);
+});
+
+// loading routes defined by route's index file
+var routes = require('./routes/index');
+
+// load mongoose and connect to the DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/MusicTwin2');
+
+// start running express and save the configruations
+// with the app variable
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// insert middleware that points to our route definitions
+
+// DEFINED ROUTES ARE IN HERE >> routes, ie './routes/index'
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
