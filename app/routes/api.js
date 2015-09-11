cd9d4fb4839d89e984363bfc7168c0e56f94404c
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-09-10 17:44:01 -0700

    changed User models file for case-sensitivity for heroku

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,5 +1,5 @@
 var bodyParser = require('body-parser');  // get body-parser
-var User       = require('../models/user');
+var User       = require('../models/User');
 var jwt        = require('jsonwebtoken');
 var config     = require('../../config');
 
